3af1d80b293a6e0fd2a167e1512631c135edb6e3
Manuel Calle <callemac88@gmail.com>
2022-11-14 13:36:42 -0500

    enviroment variables

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,13 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg'); //Mantiene las connexiones abiertas para que no se tenga que volver a hacer el proceso de connection
+const { config } = require('../config/config'); // tiene las variables de entorni de la conexión
+const USER = encodeURIComponent(config.dbUser); // se codifica el user para protegerlo
+const PASSWORD = encodeURIComponent(config.dbPassword); // se codifica el password para protegerlo
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbHost}/${config.dbName}`; //Se crea la url de conexión para hacerlo mas secillo al pasar las variables de conexión
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 50276,
-  user: 'nico',
-  password: 'admin123',
-  database: 'my_store'
-});
-
+const pool = new Pool({ connectionString: URI }); // se hace la conexión
 
 module.exports = pool;
 
